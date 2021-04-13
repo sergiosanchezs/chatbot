51ed4daf2df64e01ea826d09ba30e6b671677f9c
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2021-04-13 19:17:13 -0400

    chat messages divided by rooms

--- chat/models.py
+++ chat/models.py
@@ -7,10 +7,12 @@ User = get_user_model()
 class Message(models.Model):
   author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
   content = models.TextField()
+  room = models.TextField()
   timestamp = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
     return self.author.username
 
-  def last_10_messages():
-    return Message.objects.order_by('timestamp').all()[:10]
+  @staticmethod
+  def last_10_messages(room_name):
+    return Message.objects.filter(room=room_name).order_by('timestamp').all()[:10]

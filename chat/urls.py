394735c246b3441a084e35bd7424ae53dedf75f2
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2021-04-17 19:55:05 -0400

    update url paths

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, room
+from . import views
 
 app_name = 'chat'
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('<str:room_name>/', room, name='room'),
+    # path('', views.index, name='index'),
+    path('', views.chat, name='chat'),
+    path('chat/<str:room_name>/', views.room, name='room'),
 ]
